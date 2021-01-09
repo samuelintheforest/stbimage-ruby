e55bb7654af813390f158c429aaa817af8a0bc10
Samuel Keresztes <fellowchap-samuel@users.noreply.github.com>
2021-01-09 18:00:54 +0100

    Update stbimage.rb

--- lib/stbimage.rb
+++ lib/stbimage.rb
@@ -34,44 +34,44 @@ module STBIMAGE
   # Load native dll libary 
   def self.load_lib(lib = nil, path = nil, output_error = false)
     if lib == nil && path == nil
-      
+
       if RUBY_PLATFORM =~ /x86/
 
-        puts "You have a 32-bit Architecture ruby"
+        # puts "You have a 32-bit Architecture ruby"
         if RUBY_PLATFORM =~ /mswin/ || RUBY_PLATFORM =~ /mingw/
-          puts "With Windows"
+          # puts "With Windows"
           lib, path = 'stbDLL_x86.dll', "#{__dir__}/../dlls"
         elsif RUBY_PLATFORM =~ /linux/ || RUBY_PLATFORM =~ /cygwin/
-          puts "With Linux"
+          # puts "With Linux"
           lib, path = 'libstb_x86.so', "#{__dir__}/../dlls"
         elsif RUBY_PLATFORM =~ /darwin/
-          puts "With macOS"
+          # puts "With macOS"
         else
-          puts "I have no idea what os are you using, so it's possible that stbimage wont't work"
+          # puts "I have no idea what os are you using, so it's possible that stbimage wont't work"
         end
       
       elsif RUBY_PLATFORM =~ /arm/
       
-        puts "You have a arm architecture"
+        # puts "You have a arm architecture"
         lib, path = 'libstb_arm.so', "#{__dir__}/../dlls"
       
       elsif RUBY_PLATFORM =~ /java/
       
-        puts "You have jruby!"
+        # puts "You have jruby!"
       
       else 
       
-        puts "You have a 64-bit Architecture ruby"
+        # puts "You have a 64-bit Architecture ruby"
         if RUBY_PLATFORM =~ /mswin/ || RUBY_PLATFORM =~ /mingw/
-          puts "With Windows"
+          # puts "With Windows"
           lib, path = 'stbDLL_x64.dll', "#{__dir__}/../dlls"
         elsif RUBY_PLATFORM =~ /linux/ || RUBY_PLATFORM =~ /cygwin/
-          puts "With Linux"
+          # puts "With Linux"
           lib, path = 'libstb_x64.so', "#{__dir__}/../dlls"
         elsif RUBY_PLATFORM =~ /darwin/
-          puts "With macOS"
+          # puts "With macOS"
         else
-          puts "I have no idea what os are you using, so it's possible that stbimage wont't work"
+          # puts "I have no idea what os are you using, so it's possible that stbimage wont't work"
         end
       
       end
